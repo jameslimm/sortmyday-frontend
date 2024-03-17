4322dd3420476cacb19b128e3b951523dea20305
james <6873990+jamescode22@users.noreply.github.com>
2024-03-17 12:35:01 +0000

    Create component for filter tag, refactor filters component

---
+++ src/features/tasks/FilterTag.jsx
@@ -0,0 +1,40 @@
+const FilterTag = ({ tag, tagIndex, filterBy, setFilterBy }) => {
+  // Array of tailwind color class names to apply to
+  // the tags.
+  const TAG_COLORS = [
+    "bg-blue-100",
+    "bg-red-100",
+    "bg-green-100",
+    "bg-orange-100",
+    "bg-teal-100",
+    "bg-amber-100",
+    "bg-lime-100",
+    "bg-indigo-100",
+  ];
+
+  // apply these tailwind classes to the tag that has been selected
+  // for filtering the tasks.
+  const FILTERED_CLASS = "border-solid border-2 border-gray-900";
+
+  const formattedTag =
+    tag.substring(0, 1).toUpperCase() + tag.substring(1, tag.length).toLowerCase();
+
+  const tagColor = tagIndex >= 0 ? TAG_COLORS[tagIndex] : `bg-slate-100`;
+
+  const selectedTag = (filterBy === "" && tag === "all") || filterBy === tag;
+
+  console.log({ tag, tagIndex });
+
+  return (
+    <span
+      onClick={() => setFilterBy(tag === "all" ? "" : tag)}
+      className={`block ${tagColor} px-2 rounded-md text-sm cursor-pointer ${
+        selectedTag && FILTERED_CLASS
+      }`}
+    >
+      {formattedTag}
+    </span>
+  );
+};
+
+export default FilterTag;
--- src/features/tasks/Filters.jsx
+++ src/features/tasks/Filters.jsx
@@ -1,27 +1,11 @@
 import { useGetTasksQuery } from "../api/apiSlice";
+import FilterTag from "./FilterTag";
 
 const Filters = ({ task, filterBy, setFilterBy }) => {
   const { data: tasks } = useGetTasksQuery();
 
   // enumerate through all tasks for this user
   // and determine the unique filter tags
-
-  // Array of tailwind color class names to apply to
-  // the tags.
-  const TAG_COLORS = [
-    "bg-blue-100",
-    "bg-red-100",
-    "bg-green-100",
-    "bg-orange-100",
-    "bg-teal-100",
-    "bg-amber-100",
-    "bg-lime-100",
-    "bg-indigo-100",
-  ];
-
-  // apply these tailwind classes to the tag that has been selected
-  // for filtering the tasks.
-  const FILTERED_CLASS = "border-solid border-2 border-gray-900";
 
   if (!tasks) return;
 
@@ -31,42 +15,25 @@ const Filters = ({ task, filterBy, setFilterBy }) => {
   const tags = allUniqueTags.filter((t) => t !== "inbox");
   tags.unshift("inbox");
 
-  // Capitalise first letter of all tags
-  const displayTags = tags.map(
-    (t) => t.substring(0, 1).toUpperCase() + t.substring(1, t.length).toLowerCase()
-  );
-
   return (
     <>
       {!task && (
-        <>
-          <span
-            onClick={() => setFilterBy("")}
-            className={`block bg-slate-100 px-2 rounded-md text-sm cursor-pointer ${
-              !task && filterBy === "" && FILTERED_CLASS
-            }`}
-          >
-            All
-          </span>
-        </>
+        <FilterTag tag={"all"} tagIndex={-1} filterBy={filterBy} setFilterBy={setFilterBy} />
       )}
-      {displayTags.map((tag, i) => {
+
+      {tags.map((tag, i) => {
         // enumerate through list of all tags used by all tasks.  If no task is passed to the
         // component, output all tasks.  Otherwise, filter by the tags in this task.
-        const tagLower = tag.toLowerCase();
-        if ((task && task.tag.includes(tagLower)) || !task) {
+        if ((task && task.tag.includes(tag)) || !task)
           return (
-            <span
-              onClick={() => setFilterBy(tagLower)}
+            <FilterTag
               key={tag}
-              className={`block ${TAG_COLORS[i]} px-2 rounded-md text-sm cursor-pointer ${
-                !task && filterBy === tagLower && FILTERED_CLASS
-              }`}
-            >
-              {tag}
-            </span>
+              tag={tag}
+              tagIndex={i}
+              setFilterBy={setFilterBy}
+              filterBy={filterBy}
+            />
           );
-        }
       })}
     </>
   );
